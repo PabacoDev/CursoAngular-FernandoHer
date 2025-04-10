ce89c10162493644f37844448090e7fada999190
PabacoDev <barragancortesfrancisco@gmail.com>
2025-04-10 10:27:28 +0200

    Gifs App

---
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,76 @@
+import { HttpClient } from '@angular/common/http';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment';
+import type { GiphyResponse } from '../interfaces/giphy.intefaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
+
+const GIF_KEY = 'gifs';
+
+const loadFromLocalStorage = () => {
+  const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) ?? '{}';
+  const gifs = JSON.parse(gifsFromLocalStorage);
+  return gifs;
+};
+
+@Injectable({ providedIn: 'root' })
+export class GifService {
+  private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([]);
+
+  trendingGifsLoading = signal(true);
+
+  searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
+
+  constructor() {
+    this.loadTrendingGifs();
+  }
+
+  saveGifsToLocalStorage = effect(() => {
+    const historyString = JSON.stringify(this.searchHistory());
+    localStorage.setItem(GIF_KEY, historyString);
+  });
+
+  loadTrendingGifs() {
+    this.http
+      .get<GiphyResponse>(`${environment.giphtUrl}/gifs/trending`, {
+        params: {
+          api_key: environment.apiKey,
+          limit: 20,
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        console.log({ gifs });
+      });
+  }
+
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphtUrl}/gifs/search`, {
+        params: {
+          api_key: environment.apiKey,
+          limit: 50,
+          q: query,
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+        tap((items) => {
+          this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase()]: items,
+          }));
+        })
+      );
+  }
+
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
+  }
+}
---
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  apiKey: 'Mz0l5SUEcnmyzOZaJ9qi8yA39CgexRVT',
+  giphtUrl: 'https://api.giphy.com/v1',
+};
