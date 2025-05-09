a3dd8fa665451ba26cc43669c600fc1919ad974f
PabacoDev <franciscobarragancortes@gmail.com>
2025-05-09 12:33:41 +0200

    Teslo-Shop hasta autenticación y autorización

---
+++ 09-teslo-shop/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [NotAuthenticatedGuard],
+  },
+  {
+    path: '',
+    loadChildren: () => import('./store-front/store-front.routes'),
+  },
+];
---
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import {
+  Product,
+  ProductsResponse,
+} from '@products/interfaces/product.interface';
+import { delay, Observable, of, tap } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+  private http = inject(HttpClient);
+
+  private productsCache = new Map<string, ProductsResponse>();
+
+  private productCache = new Map<string, Product>();
+
+  getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 12, offset = 0, gender = '' } = options;
+
+    const key = `${limit}-${offset}-${gender}`;
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
+    return this.http
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit: limit,
+          offset: offset,
+          gender: gender,
+        },
+      })
+      .pipe(
+        tap((resp) => console.log(resp)),
+        tap((resp) => this.productsCache.set(key, resp))
+      );
+  }
+
+  getProductByIdSlug(idSlug: string): Observable<Product> {
+    const key = `${idSlug}`;
+    if (this.productCache.has(key)) {
+      return of(this.productCache.get(key)!);
+    }
+    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
+      delay(2000),
+      tap((resp) => this.productCache.set(key, resp))
+    );
+  }
+}
